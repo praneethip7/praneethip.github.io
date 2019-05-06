e05a0537ad7240d76010fbba78e57560e026ed74
praneeth <praneeth.56482@gmail.com>
2019-05-06 11:59:20 -0700

    updated index.html

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 function getDetails(){
-    console.log('Im here');
-    
-    var city = document.getElementById('city').value;
-    console.log('Im here' + city);
 
+    var city = document.getElementById('city').value;
+    var api = "&APPID=4e8fe55b900263c5f83603ed631e15ad";
     $.ajax({
-        url:'http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=4e8fe55b900263c5f83603ed631e15ad',
+        url:'http://api.openweathermap.org/data/2.5/weather?q='+city+api,
         success: function(data){
             console.log(data);
-            document.getElementById('name').innerHTML=JSON.stringify(data);
+            document.getElementById('name').innerHTML=JSON.stringify(data.weather);
         },
         error:function(error){
             console.log(error);
